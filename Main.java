1f79085c850dd7be93e2a67a914d7b3ccc659ca7
codytheking <codytheking93@gmail.com>
2021-12-03 11:30:26 -0800

    Starter

---
+++ Main.java
@@ -0,0 +1,37 @@
+/**
+ * A30
+ *
+ * @author
+ * @version
+ */
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("How many values do you have? ");
+        int n = in.nextInt();
+
+        // create an array of ints with the identifier "values" ( or get user input).
+
+        
+        for(int i = 0; i < values.length; i++)
+        {
+            System.out.print("Enter value: ");
+            values[i] = in.nextInt();
+        }
+
+        // call maxVal method and print result
+    }
+}
+
+/** 
+ * maxVal
+ * @param nums - array of ints
+ * @return largest int value
+ */
+
+
